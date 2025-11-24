ce64ddc836cc0a482eb6372a6a12d31eaf58fa1d
GeorginaShirley07 <georginashirley07@gmail.com>
2025-11-24 21:36:49 +0000

    defender class

--- Defender.py
+++ Defender.py
@@ -1,13 +1,13 @@
-import pygame, sys
+import pygame
 
 class Block(pygame.sprite.Sprite): #A block class for the barriers
     def __init__(self,size,color,x,y):
         super().__init__()
-        self.image = pygame.Surface([size,size])
+        self.image = pygame.Surface((size,size))
         self.image.fill(color)
         self.rect = self.image.get_rect(topleft = (x,y))
 # Shape in the format of a list of strings
-SHAPE = [
+shape = [
     "  XXXXXXXX  ",       
     " XXXXXXXXXX ",
     "XXXXXXXXXXXX",
