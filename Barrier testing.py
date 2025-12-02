0c7b488b49eac1eeed20c5956416eca1bc666318
GeorginaShirley07 <georginashirley07@gmail.com>
2025-12-02 11:57:41 +0000

    Tested barrier, it is in correct position. need to make more

--- Barrier testing.py
+++ Barrier testing.py
@@ -6,15 +6,17 @@ screen = pygame.display.set_mode((500,500))
 clock = pygame.time.Clock()
 screen.fill((30,30,30))
 
-barrier = Barrier()
+barrier = Barrier(450,450)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            sys.exit()
+            
     
     barrier.blocks_group.draw(screen)
     
     pygame.display.flip()
     clock.tick(60)
+
+sys.exit()
--- Barriers.py
+++ Barriers.py
@@ -2,26 +2,28 @@ import pygame
 
 class Block(pygame.sprite.Sprite): #A block class for the barriers
     def __init__(self,x,y):
-        super().__init__()
-        self.image = pygame.Surface((3,3))
+        super().__init__() #Initialize the sprite
+        self.image = pygame.Surface((10,10)) #Create a 3x3 pixel block
         self.image.fill((0,225,0))
-        self.rect = self.image.get_rect(topleft = (x,y))
+        self.rect = self.image.get_rect(topleft = (x,y)) #Set position of blocks
 # Shape in the format of a list of strings
 shape = [
-    "  XXXXXXXX  ",       
-    " XXXXXXXXXX ",
-    "XXXXXXXXXXXX",
-    "XXX      XXX",
-    "XX        XX",]
+    "   XXXXXXXXXXXXXXX  ",       
+    "  XXXXXXXXXXXXXXXXX ",
+    " XXXXXXXXXXXXXXXXXXX",
+    "XXXXXXXXXXXXXXXXXXXXX",
+    " XXXXXXXXXXXXXXXXXXX",
+    " XXXXXX       XXXXXX",
+    " XXX             XXX",]
 shape = [list(row) for row in shape] #Convert each string to a list of characters
 
 class Barrier:
-    def __init__(self):
-        self.blocks_group = pygame.sprite.Group()
+    def __init__(self, x, y): #A barrier class to create barriers from blocks
+        self.blocks_group = pygame.sprite.Group() #Group to hold all the blocks
         for row in range(len(shape)):
             for col in range(len(shape[0])):
-                if shape[row][col] == "X":
-                    x = col * 3
-                    y = row * 3
-                    block = Block(x,y)
+                if shape[row][col] == "X": #If the character is an X, create a block
+                    x_pos = x + col * 3 #calculate x and y position on screen
+                    y_pos = y + row * 3
+                    block = Block(x_pos,y_pos) 
                     self.blocks_group.add(block) #Add block to the group
